c16e9fca001c6ea5cfd394027d4a206831d2551a
Kylin <394145332@qq.com>
2020-10-04 23:21:53 +0800

    update

--- pcr-favor.py
+++ pcr-favor.py
@@ -13,27 +13,31 @@ print('start')
 os.chdir('C:/Program Files (x86)/MuMu/emulator/nemu/vmonitor/bin')
 
 num = 0
-os.system('adb_server.exe shell input tap 510 770')
+os.system('adb_server.exe kill-server')
+
+os.system('adb_server.exe connect 127.0.0.1:7555')
+
+os.system('adb_server.exe -s 127.0.0.1:7555 shell input tap 510 770')
 time.sleep(1)
-os.system('adb_server.exe shell input tap 830 550')
+os.system('adb_server.exe -s 127.0.0.1:7555 shell input tap 830 550')
 
 while num < total_times:      
     time.sleep(1)
-    os.system('adb_server.exe shell input tap 900 190')
+    os.system('adb_server.exe -s 127.0.0.1:7555 shell input tap 900 190')
     time.sleep(1)
-    os.system('adb_server.exe shell input tap 950 300')
+    os.system('adb_server.exe -s 127.0.0.1:7555 shell input tap 950 300')
     time.sleep(1)
-    os.system('adb_server.exe shell input tap 720 560')
+    os.system('adb_server.exe -s 127.0.0.1:7555 shell input tap 720 560')
     time.sleep(2)
-    os.system('adb_server.exe shell input tap 1380 60')
+    os.system('adb_server.exe -s 127.0.0.1:7555 shell input tap 1380 60')
     time.sleep(1)
-    os.system('adb_server.exe shell input tap 1210 70')
+    os.system('adb_server.exe -s 127.0.0.1:7555 shell input tap 1210 70')
     time.sleep(1)
-    os.system('adb_server.exe shell input tap 880 560')
+    os.system('adb_server.exe -s 127.0.0.1:7555 shell input tap 880 560')
     time.sleep(3)
-    os.system('adb_server.exe shell input tap 720 720')
+    os.system('adb_server.exe -s 127.0.0.1:7555 shell input tap 720 720')
     time.sleep(1)
-    os.system('adb_server.exe shell input tap 55 50')
+    os.system('adb_server.exe -s 127.0.0.1:7555 shell input tap 55 50')
     num += 1
     
 print('end')
